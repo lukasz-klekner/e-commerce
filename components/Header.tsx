e82dd8fc41d0d3ae43a24dce1551beb3842bd6e3
Łukasz Klekner <lukaszklekner@gmail.com>
2022-10-08 22:20:09 +0200

    adds navigation with links and installs clsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,30 @@
-export const Header = () => (
-    <header className="bg-gray-900 text-white px-4 py-2">
+import Link from "next/link";
+import { useRouter } from "next/router";
+import clsx from 'clsx'
+
+export const Header = () => {
+  const { asPath } = useRouter()
+
+  return (
+  <header className="bg-gray-900 text-white px-4 py-2">
     <nav>
-      Nawigacja
+      <ul className="flex gap-2">
+        <li>
+            <Link href='/'>
+                <a className={`${clsx(asPath === '/' && 'underline')}`}>
+                    Home
+                </a>
+            </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+                  <a className={`${clsx(asPath === '/about' && 'underline')}`}>
+                      About
+                  </a>
+              </Link>
+        </li>
+      </ul>
     </nav>
   </header>
 )
+}
