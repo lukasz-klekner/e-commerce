4e6861438f9d4f7b24a3ce8f629dba8a80050a53
Łukasz Klekner <lukaszklekner@gmail.com>
2022-10-09 17:25:12 +0200

    fixes Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,78 @@
-export const Footer = () => (
-  <footer className="bg-gray-900 text-white px-4 py-2">
-    Stopka
-  </footer>
-)
+import { Logo } from "../icons/Logo"
+import { SocialMediaItem } from "./SocialMediaItem"
+
+const socialMediaItems = [
+  { name: 'facebook', href: '/'},
+  { name: 'instagram', href: '/'},
+  { name: 'twitter', href: '/'},
+  { name: 'github', href: '/'},
+]
+
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-100">
+      <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="flex justify-center text-teal-600">
+          <Logo />
+        </div>
+
+        <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
+          consequuntur amet culpa cum itaque neque.
+        </p>
+
+        {/* <nav className="mt-12" aria-labelledby="footer-navigation">
+          <h2 className="sr-only" id="footer-navigation">Footer navigation</h2>
+
+          <ul className="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                Careers
+              </a>
+            </li>
+
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                History
+              </a>
+            </li>
+
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                Services
+              </a>
+            </li>
+
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                Projects
+              </a>
+            </li>
+
+            <li>
+              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav> */}
+
+        <ul className="mt-12 flex justify-center gap-6 md:gap-8">
+          {
+            socialMediaItems.map(({ href, name}) => (
+              <li key={name}>
+                <SocialMediaItem href={href} name={name} />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </footer>
+  )
+}
---
+++ components/SocialMediaItem.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+import { Facebook } from "../icons/Facebook";
+import { GitHub } from "../icons/Github";
+import { Instagram } from "../icons/Instagram";
+import { Twitter } from "../icons/Twitter";
+
+interface SocialMediaItemProps {
+    href: string
+    name: string
+}
+
+const getIconFromName = (iconName: string) => {
+    switch(iconName) {
+        case 'facebook':
+            return <Facebook />;
+        case 'instagram':
+            return <Instagram />;
+        case 'twitter':
+            return <Twitter />;
+        case 'github':
+            return <GitHub />;
+    }
+}
+
+export const SocialMediaItem = ({ href, name }: SocialMediaItemProps) => {
+    const icon = getIconFromName(name)
+
+    return(
+        <Link href={href}>
+            <a
+                href="/"
+                rel="noreferrer"
+                target="_blank"
+                className="text-gray-700 transition hover:text-gray-700/75"
+            >
+                <span className="sr-only">{name}</span>
+                {icon}
+            </a>
+        </Link>
+    )
+}
