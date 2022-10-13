e74a29e17672f23a6ec38b9b53941cdb4929123b
Łukasz Klekner <lukaszklekner@gmail.com>
2022-10-13 09:13:11 +0200

    fetches data per page and integrates with pagination

--- pages/products-csr.tsx
+++ pages/products-csr.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
 import { useQuery } from 'react-query'
+
+import { Pagination } from '../components/Pagination'
 import { ProductDetails } from '../components/Product'
 
 interface StoreAPIResponse {
@@ -14,15 +17,28 @@ interface StoreAPIResponse {
   }
 }
 
-const getProducts = async () => {
-  const response = await fetch('https://fakestoreapi.com/products')
+const LIMIT = 25
+
+const getProducts = async (page: number) => {
+  const response = await fetch(
+    `https://naszsklep-api.vercel.app/api/products?take=${LIMIT}&offset=${
+      (page - 1) * 25
+    }`
+  )
   const data: StoreAPIResponse[] = await response.json()
 
   return data
 }
 
 const ProductsCSRPage = () => {
-  const { data, isLoading, error } = useQuery('products', getProducts)
+  const [currentPage, setCurrentPage] = useState(1)
+  const { data, isLoading, error } = useQuery(
+    ['products', currentPage],
+    () => getProducts(currentPage),
+    {
+      keepPreviousData: true,
+    }
+  )
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -33,22 +49,28 @@ const ProductsCSRPage = () => {
   }
 
   return (
-    <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-      {data.map(({ id, description, image, title, rating }) => (
-        <li key={id} className='shadow-xl border-2 p-4'>
-          <ProductDetails
-            data={{
-              id,
-              description,
-              title,
-              thumbnailAlt: title,
-              rating: rating.rate,
-              thumbnailUrl: image,
-            }}
-          />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+        {data.map(({ id, description, image, title, rating }) => (
+          <li key={id} className='shadow-3xl border-2 p-4'>
+            <ProductDetails
+              data={{
+                id,
+                description,
+                title,
+                thumbnailAlt: title,
+                rating: rating.rate,
+                thumbnailUrl: image,
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        currentPage={currentPage}
+        onPageChange={(page: number) => setCurrentPage(page)}
+      />
+    </div>
   )
 }
 
