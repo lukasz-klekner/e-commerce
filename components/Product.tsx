a1f1db7a29a1e96857da4b4fc379098e8b3bb49d
Łukasz Klekner <lukaszklekner@gmail.com>
2022-10-14 08:32:12 +0200

    fix: optimalize images and configurate next

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
 import { Rating } from './Rating'
 
 interface ProductProps {
@@ -16,7 +18,16 @@ interface ProductDetails {
 export const ProductDetails = ({ data }: ProductProps) => {
   return (
     <>
-      <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
+      <div className='bg-white p-8'>
+        <Image
+          src={data.thumbnailUrl}
+          alt={data.thumbnailAlt}
+          layout='responsive'
+          width={16}
+          height={9}
+          objectFit='contain'
+        />
+      </div>
       <div>
         <h2 className='p-4 text-2xl text-bold'>{data.title}</h2>
         <p className='p-4'>{data.description}</p>
@@ -38,7 +49,16 @@ interface ProductListItemProps {
 export const ProductListItem = ({ data }: ProductListItemProps) => {
   return (
     <>
-      <img src={data.thumbnailUrl} alt={data.thumbnailAlt} />
+      <div className='bg-white p-8'>
+        <Image
+          src={data.thumbnailUrl}
+          alt={data.thumbnailAlt}
+          layout='responsive'
+          width={16}
+          height={9}
+          objectFit='contain'
+        />
+      </div>
       <Link href={`products/${data.id}`}>
         <a>
           <h2 className='p-4 text-2xl text-bold'>{data.title}</h2>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['picsum.photos', 'naszsklep-api.vercel.app'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
