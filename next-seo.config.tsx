cff41b6eb1c5602791afa22cffade87817ef6cff
Łukasz Klekner <lukaszklekner@gmail.com>
2022-10-19 08:53:11 +0200

    adds Next-Seo

---
+++ next-seo.config.tsx
@@ -0,0 +1,13 @@
+const title = 'E-commerce by Łuki'
+const description = 'Pototype e-commerce website created by Łuki during course'
+
+const defaultSeoConfig = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+  },
+}
+
+export default defaultSeoConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 import '../styles/globals.css'
 import { Layout } from '../components/Layout'
+import SEO from '../next-seo.config'
 
 const client = new QueryClient()
 
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
       <Layout>
+        <DefaultSeo {...SEO} />
         <Component {...pageProps} />
       </Layout>
     </QueryClientProvider>
