1972930b2385d24016929b058e2a3baafcd548ec
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-08 18:16:03 +0100

    feat: integrate apollo with shop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,28 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import { ApolloProvider } from '@apollo/client'
 
 import '../styles/globals.css'
 import { Layout } from '../components/Layout'
 import SEO from '../next-seo.config'
 import { CartStateContextProvider } from '../components/Cart/CartContext'
+import { apolloClient } from '../graphql/apolloClient'
 
 const client = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CartStateContextProvider>
-      <QueryClientProvider client={client}>
-        <Layout>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </Layout>
-      </QueryClientProvider>
-    </CartStateContextProvider>
+    <ApolloProvider client={apolloClient}>
+      <CartStateContextProvider>
+        <QueryClientProvider client={client}>
+          <Layout>
+            <DefaultSeo {...SEO} />
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </CartStateContextProvider>
+    </ApolloProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
+import { useQuery, gql } from '@apollo/client'
+
 const Home = () => {
+  const { data, loading, error } = useQuery(gql`
+    query getProductsList {
+      products {
+        id
+        slug
+        name
+        price
+      }
+    }
+  `)
+
+  console.log(data)
   return (
     <div className='bg-blue-500 text-white flex-grow p-6 gap-6 grid sm:grid-cols-2'>
       <div>E-commerce by Łuki</div>
