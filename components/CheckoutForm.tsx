d135d2a1a8157438afbf0436807fa452730cb6a5
Łukasz Klekner <lukaszklekner@gmail.com>
2022-11-15 18:42:04 +0100

    fix: translate errors for PL

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,9 +1,39 @@
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-import { InferType } from 'yup'
+import { InferType, setLocale } from 'yup'
 
 import { FormInput } from './FormInput'
+
+setLocale({
+  mixed: {
+    required: 'To pole jest wymagane',
+  },
+  string: {
+    // length: 'Deve ter exatamente ${length} caracteres',
+    // min: 'Deve ter pelo menos ${min} caracteres',
+    // max: 'Deve ter no máximo ${max} caracteres',
+    email: 'Niepoprawny email',
+  },
+  number: {
+    // min: 'Deve ser no mínimo ${min}',
+    // max: 'Deve ser no máximo ${max}',
+    // lessThan: 'Deve ser menor que ${less}',
+    // moreThan: 'Deve ser maior que ${more}',
+    // notEqual: 'Não pode ser igual à ${notEqual}',
+    // positive: 'Deve ser um número positivo',
+    // negative: 'Deve ser um número negativo',
+    // integer: 'Deve ser um número inteiro',
+  },
+  //   date: {
+  //     min: 'Deve ser maior que a data ${min}',
+  //     max: 'Deve ser menor que a data ${max}',
+  //   },
+  //   array: {
+  //     min: 'Deve ter no mínimo ${min} itens',
+  //     max: 'Deve ter no máximo ${max} itens',
+  //   },
+})
 
 const checkoutFormSchema = yup.object({
   firstName: yup.string().required(),
