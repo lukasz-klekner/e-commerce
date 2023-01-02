13439093a1fa4c540cb2711555cd9563a49d151f
Luki Klekner <lukaszklekner@gmail.com>
2023-01-02 11:41:33 +0100

    fix: add styles for review's list

--- components/ProductReviewItem.tsx
+++ components/ProductReviewItem.tsx
@@ -7,11 +7,11 @@ interface ProductReviewItemProps {
 export const ProductReviewItem = ({ review }: ProductReviewItemProps) => {
   
     return (
-        <li>
-            <h3>{review.content}</h3>
-            <p>{review.content}</p>
-            <div>{review.rating}</div>
-            <footer>{review.name}</footer>
+        <li className="border mt-4 p-4 bg-white max-w-md mx-auto shadow-md rounded-md">
+            <h3 className="font-bold">{review.headline}</h3>
+            <p className="my-2 italic">{review.content}</p>
+            <div className="text-gray-600">{review.rating}</div>
+            <footer className="pt-4">{review.name}</footer>
         </li>
     )
   }
