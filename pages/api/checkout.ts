3919e03d30023cc9d6bb5b5a30bfafd4c5aaefcd
Luki Klekner <lukaszklekner@gmail.com>
2023-01-06 15:38:58 +0100

    feat: install stripe and add checkout route

---
+++ pages/api/checkout.ts
@@ -0,0 +1,40 @@
+import { NextApiHandler } from "next";
+import { Stripe } from "stripe";
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
+
+const checkoutHandler: NextApiHandler = async (req, res) => {
+    if(!STRIPE_SECRET_KEY){
+        res.status(500).json({ message: 'Missing STRIPE_SECRET_KEY'})
+        return
+    }
+
+    const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        locale: 'pl',
+        payment_method_types: ['p24', 'card'],
+        success_url: 'http://localhost:3000/checkout/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'http://localhost:3000/checkout/cancel',
+        line_items: [
+            {
+                price_data: {
+                    currency: 'PLN',
+                    unit_amount: 2137,
+                    product_data: {
+                        name: 'T-Shirt',
+                    }           
+                },
+                quantity: 5
+            }
+        ]
+    })
+
+    res.status(201).json({
+        session: stripeCheckoutSession
+    })
+
+}
+
+export default checkoutHandler;
