38dc32f2310cf88f21b3107ba45be3afc6d8644d
Luki Klekner <lukaszklekner@gmail.com>
2023-01-11 10:13:33 +0100

    feat: create signup page

--- components/Header.tsx
+++ components/Header.tsx
@@ -64,7 +64,7 @@ export const Header = () => {
                 </Link> */}
 
                 <div className='hidden sm:flex'>
-                  <Link href='/'>
+                  <Link href='/signup'>
                     <a className='rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75'>
                       Register
                     </a>
---
+++ pages/signup.tsx
@@ -0,0 +1,80 @@
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { InferType } from 'yup'
+
+const signupFormSchema = yup.object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  
+type SignupFormData = InferType<typeof signupFormSchema>
+
+const SingupPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<SignupFormData>({
+        resolver: yupResolver(signupFormSchema),
+      })
+    
+      const onSubmit = handleSubmit(data => console.log(data))
+
+    return (
+        <section>
+        <h1 className='sr-only'>Set up an account</h1>
+  
+            <div className='bg-white py-12 md:py-24'>
+              <div className='mx-auto max-w-lg px-4 lg:px-8'>
+                <form className='flex flex-col' onSubmit={onSubmit}>
+                  <div className='mb-4 flex items-center gap-2'>
+                      <label className='mb-1 block text-sm text-gray-600' htmlFor='email'>
+                          Email
+                      </label>
+  
+                      <input
+                          className='w-full rounded-lg border-gray-200 p-2.5 text-sm shadow-sm placeholder-gray-400'
+                          type='email'
+                          id='email'
+                          {...register(`email`, { required: true})}
+                      />
+  
+                      <span role='alert' className='text-red-500 text-xs'>
+                          {errors.email?.message}
+                      </span>
+                  </div>
+
+                  <div className='mb-4 flex items-center gap-2'>
+                      <label className='mb-1 block text-sm text-gray-600' htmlFor='password'>
+                          Password
+                      </label>
+  
+                      <input
+                          className='w-full rounded-lg border-gray-200 p-2.5 text-sm shadow-sm placeholder-gray-400'
+                          type='password'
+                          id='password'
+                          {...register(`password`, { required: true})}
+                      />
+  
+                      <span role='alert' className='text-red-500 text-xs'>
+                          {errors.password?.message}
+                      </span>
+                  </div>
+  
+                  <div>
+                    <button
+                      className='block w-full rounded-lg bg-black p-2.5 text-sm text-white'
+                      type='submit'
+                    >
+                      Set up an account!
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+      </section>
+    )
+}
+
+export default SingupPage
